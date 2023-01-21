43d38bffaa09152f3e348762c4074921fdf1d986
fannilin <103945012+fannilin@users.noreply.github.com>
2023-01-21 20:19:23 +0200

    valmis

--- App.js
+++ App.js
@@ -1,14 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, Text, View, Alert, TextInput } from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [text, setText] = useState('');
+  
+  const buttonPressed= () => {
+    Alert.alert('kirjoitit: '+ text);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Laskin sovellus
+      </Text>
+      <TextInput 
+      style={{width: 200, borderColor: 'blue', borderWidth: 1}}
+      onChangeText={text => setText(text)} value={text}/>
+      <Button onPress={buttonPressed} title='paina tätä'/>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+
+
+
 
 const styles = StyleSheet.create({
   container: {
